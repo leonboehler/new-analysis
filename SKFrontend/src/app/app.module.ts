60549b29d3510dcab8783d102ac9a868ecfba488
Philipp Kappus <philipp.kappus@gmail.com>
2020-03-22 20:02:55 +0100

    moved map component

--- SKFrontend/src/app/app.module.ts
+++ SKFrontend/src/app/app.module.ts
@@ -2,25 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './components/test/test.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AboutComponent } from './components/about/about.component';
 import { AgbComponent } from './components/agb/agb.component';
 import { DataProtectionComponent } from './components/data-protection/data-protection.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { MapComponent } from './map/map.component';
+import { MapComponent } from './components/map/map.component';
 import { SettingsComponent } from './components/settings/settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
     FooterComponent,
     AboutComponent,
     AgbComponent,
     DataProtectionComponent,
     ToolbarComponent,
-    TestComponent,
     MapComponent,
     SettingsComponent
   ],
