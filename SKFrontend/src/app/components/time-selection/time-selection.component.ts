a48f1f9ee95994c6d187f435e779bbfc68105319
Philipp Kappus <philipp.kappus@gmail.com>
2020-03-25 15:46:04 +0100

    finished time selection

--- SKFrontend/src/app/components/time-selection/time-selection.component.ts
+++ SKFrontend/src/app/components/time-selection/time-selection.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {range} from 'rxjs';
 
 @Component({
@@ -8,11 +8,10 @@ import {range} from 'rxjs';
 })
 export class TimeSelectionComponent implements OnInit {
 
-  times = new Array<{label: string; value: number}>()
-  selectedTime = {label: '00:00', value: 0};
-  selectedTimes = new Array<{label: string; value: number}>()
-  selectedTimesList = new Array<number>()
-  @Output() selectedTimesValues = new EventEmitter();
+  times = new Array<string>()
+  selectedTime = '00:00';
+  @Input() selectedTimes = new Array<string>()
+  @Output() valueChanged = new EventEmitter();
 
   constructor() {
     range(0, 24 ).subscribe(hour => {
@@ -23,22 +22,22 @@ export class TimeSelectionComponent implements OnInit {
       } else {
         label = '0' + hour;
       }
-      let value = hour * 10;
-      this.times.push({label: `${ label }:00`,value: value});
-
-      value = hour * 10 + 30;
-      this.times.push({label: `${ label }:30`,value: value});
+      this.times.push(`${ label }:00`);
+      this.times.push(`${ label }:30`);
     });
   }
 
   ngOnInit(): void {
   }
 
+  timeToLabel(time: number) {
+
+  }
+
   onAddButtonClick() {
     if (this.selectedTimes.indexOf(this.selectedTime) === -1) {
       this.selectedTimes.push(this.selectedTime);
-      this.selectedTimesList.push(this.selectedTime.value)
-      this.selectedTimesValues.emit(this.selectedTimesList)
+      this.valueChanged.emit(this.selectedTimes)
     }
   }
 
