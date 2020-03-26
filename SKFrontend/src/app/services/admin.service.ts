2837accee654b9650f594fb011452fc71f64c9c9
Philipp Kappus <philipp.kappus@gmail.com>
2020-03-26 13:02:46 +0100

    create-location

--- SKFrontend/src/app/services/admin.service.ts
+++ SKFrontend/src/app/services/admin.service.ts
@@ -16,6 +16,9 @@ export class AdminService {
   currentBucket = new BehaviorSubject<Bucket>(null)
 
   selectedLocation = new BehaviorSubject<Location>(null)
+
+  drawMode = new BehaviorSubject<string>('none')
+
   constructor() { }
 
   setCurrentBucket(bucket: Bucket) {
@@ -49,6 +52,10 @@ export class AdminService {
     const index = this.createdBucketsData.indexOf(bucket)
     this.createdBucketsData[index] = bucket
     this.createdBuckets.next(this.createdBucketsData)
+  }
+
+  setDrawMode(mode: string){
+    this.drawMode.next(mode)
   }
 
   setSelectedLocation(location: Location) {
