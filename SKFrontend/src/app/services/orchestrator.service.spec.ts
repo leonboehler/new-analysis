6a6a25e1e3c538d74146ff777087368f3bd786e8
matztron <muschmatthias@gmail.com>
2020-03-28 14:07:25 +0100

    Added test TK-0202a (validate maturity function)

--- SKFrontend/src/app/services/orchestrator.service.spec.ts
+++ SKFrontend/src/app/services/orchestrator.service.spec.ts
@@ -16,4 +16,22 @@ describe('OrchestratorService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should not allow person younger than 18 years (TK-0202a; D10)', () => {
+    var currentYear = new Date().getFullYear();
+
+    //In case of 2020: Test for 2010 (as year of birth)
+    var res = service.validBirthday(String(currentYear - 10));
+
+    expect(res).toBe(false);
+  });
+
+  it('should allow person older than 18 years (TK-0202a; D10)', () => {
+    var currentYear = new Date().getFullYear();
+
+    //In case of 2020: Test for 1997 (as year of birth)
+    var res = service.validBirthday(String(currentYear - 22));
+
+    expect(res).toBe(true);
+  });
 });
