a1dfc578712ef9469fce560cd950701b3e693d0c
sewpalu <sewpalu@gmail.com>
2020-03-29 13:32:21 +0200

    Add tests concerning Login / -out
    TK-0219a, TK-0221a

---
+++ RestTest/RestTest.Tests/UsersessionTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FroggyRestServer.Controllers;
+using System.Collections.Generic;
+using RestTest.Tests;
+
+namespace FroggyRestServer.Tests
+{
+    [TestClass]
+    public class UsersessionTest
+    {
+
+        [TestMethod]
+        public void loginWorks() // TK-0219a
+        {
+            var user = Utilitities.addTestUser();
+            var userController = new UserController();
+
+            var result = userController.Login(user);
+            var userSessions = (List<Dictionary<string, object>>)MySQLConnector.ConExecuteReaderMany(
+                $"SELECT end_ts FROM log_session WHERE session_id LIKE '{(string)((Dictionary<string, object>)result["data"])["token"]}';")["data"];
+
+            Assert.AreEqual(result["code"], 200, "Login unsuccessful");
+            Assert.AreEqual(userSessions.Count, 1, "Usersession wasn't added or isn't unique");
+            Assert.AreEqual(userSessions[0]["end_ts"], DBNull.Value, "Usersession was already ended");
+
+            userController.Logout(user);
+        }
+
+        [TestMethod]
+        public void logoutWorks() // TK-0221a
+        {
+            var user = Utilitities.addTestUser();
+            var userController = new UserController();
+
+            userController.Logout(user); // < v Ensure, we've got a usable user session
+            var sessionToken = (string)((Dictionary<string, object>)userController.Login(user)["data"])["token"];
+            var result = userController.Logout(user);
+            var openSessions = (List<Dictionary<string, object>>)MySQLConnector.ConExecuteReaderMany(
+                $"SELECT NULL FROM log_session WHERE end_ts IS NULL AND session_id LIKE '{sessionToken}';")["data"];
+
+            Assert.AreEqual(result["code"], 200, "Logout unsuccessful");
+            Assert.AreEqual(openSessions.Count, 0, "Usersession wasn't ended on logout");
+        }
+    }
+}
